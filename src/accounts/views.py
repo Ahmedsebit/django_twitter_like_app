f94a5f385bd61a32270de6e3b741d9485c329212
Ahmedsebit <Ahmed.yusuf@andela.com>
2017-07-13 16:06:26 +0300

    Added Follow functionality

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth import get_user_model
-from django.views.generic import DetailView
-from django.shortcuts import render, get_object_or_404
 
+from django.views import View
+from django.views.generic import DetailView
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import UserProfile
 # Create your views here.
 
 User = get_user_model()
@@ -13,3 +17,18 @@ class UserDetailView(DetailView):
 
     def get_object(self):
         return get_object_or_404(User, username__iexact=self.kwargs.get("username"))
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(UserDetailView, self).get_context_data(*args, **kwargs)
+        following = UserProfile.object.is_following(self.request.user, self.get_object())
+        context['following'] = following
+        return context
+
+class UserFollowView(View):
+    def get(self, request, username, *args, **kwargs):
+        toggle_user = get_object_or_404(User, username__iexact=username)
+        if request.user.is_authenticated():
+            is_folowing = UserProfile.object.toggle_follow(request.user, toggle_user)
+
+        return redirect("profiles:detail", username=username)
+
